f5eae8fec609fae9dad04efe94b445d93d7fb3eb
eve-link <eve.link.1992@gmail.com>
2014-09-29 23:31:24 +0300

    argumendita funktsioon

---
+++ harjutus-9.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Harjutus-9</title>
+</head>
+<body>
+<h2>Funktsiooni defineerimine</h2>
+<h3>Argumendita funktsioon</h3>
+
+<?php 
+  function say_hello() {
+      echo 'Minu esimene funktsioon!<br>';
+  }
+ ?>
+
+
+</body>
+</html>
